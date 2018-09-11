41864016180774c37bc8aec6d4e3a682c98bdb1b
Erik Stach <e.stach@elo.com>
2018-09-11 18:57:21 +0200

    removed https

--- config.js
+++ config.js
@@ -5,11 +5,9 @@ var defaultEnv = "staging",
     environments = {
       staging: {
         httpPort: 3000,
-        httpsPort: 3001
       },
       production: {
         httpPort: 5000,
-        httpsPort: 5001
       }
     };
 
--- index.js
+++ index.js
@@ -3,26 +3,13 @@
  */
 
 var http = require("http"),
-    https = require("https"),
-    fs = require("fs"),
     unifiedServer = require("./server"),
-    config = require("./config"),
-    httpsServerOptions = {
-      key: fs.readFileSync("./https/key.pem"),
-      cert: fs.readFileSync("./https/cert.pem")
-    },
-    httpServer, httpsServer;
+    config = require("./config");
 
-(httpServer = http.createServer(function (req, res) {
-  unifiedServer(req, res);
-}))
-.listen(config.httpPort, function () {
-  console.log("The server is listening on http port " + config.httpPort);
-});
-
-(httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-  unifiedServer(req, res);
-}))
-.listen(config.httpsPort, function () {
-  console.log("The server is listening on https port " + config.httpsPort);
-});
+http
+  .createServer(function (req, res) {
+    unifiedServer(req, res);
+  })
+  .listen(config.httpPort, function () {
+    console.log("The server is listening on http port " + config.httpPort);
+  });
